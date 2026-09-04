8bcd28f7e7e83ebcb8f2ec97989fe8978d60b24e
DC* <dario.cavuotti@gmail.com>
2026-09-04 23:55:22 +0100

    Update config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -9,13 +9,7 @@ return {
       preset = "modern",
     },
     keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
+      {},
     },
   },
 }
